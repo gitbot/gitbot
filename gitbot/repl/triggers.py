4d66c40d7246733f8098fb647c3b6a18bd067585
Lakshmi Vyasarajan <lakshmi.vyas@gmail.com>
2013-02-25 00:46:46 +0530

    Add repl function to trigger gitbot status

--- gitbot/repl/triggers.py
+++ gitbot/repl/triggers.py
@@ -24,3 +24,22 @@ def trigger_action(proj, repo, branch, sha, event='push'):
     else:
         print 'Cannot trigger build'
         print response.text
+
+def trigger_status(build_id, status, url=None, message=None):
+    url_templ = 'http://api.gitbot.io/hooks/jobs/{build}/status'
+    data = dict(
+        state=status,
+        message=message,
+        url=url
+    )
+    headers = {
+        "Content-type": "application/json",
+        "Accept": "text/plain"
+    }
+    url = url_templ.format(build=urllib.quote_plus(build_id))
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    if response.status_code == 200:
+        print 'Status set successfully.'
+    else:
+        print 'Cannot set status'
+        print response.text
